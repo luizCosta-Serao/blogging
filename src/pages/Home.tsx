eaa127aafbfbf4bbc424b988095e1d1e511d19fd
Luiz Serão <luiz.serao@hotmail.com>
2024-01-29 09:30:37 -0300

    banner comunidade da página inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import WelcomeHome from '../components/WelcomeHome/WelcomeHome'
 import PostsHome from '../components/PostsHome/PostsHome'
+import BannerCommunity from '../components/BannerCommunity'
 
 const Home = () => {
   return (
     <section>
       <WelcomeHome />
       <PostsHome />
+      <BannerCommunity />
     </section>
   )
 }
