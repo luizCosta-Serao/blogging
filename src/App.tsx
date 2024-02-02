7360970f9ab78ead1c870964c0bb674f8f812786
Luiz Serão <luiz.serao@hotmail.com>
2024-02-02 13:21:37 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import { PostsContextProvider } from './context/PostsContext'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </PostsContextProvider>
   )
