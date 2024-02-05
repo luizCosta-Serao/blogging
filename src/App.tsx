bea243f2fcf6b969657d6320d581618a44b1187c
Luiz Serão <luiz.serao@hotmail.com>
2024-02-05 09:55:01 -0300

    páginas para posts de cada categoria

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import Home from './pages/Home'
 import { PostsContextProvider } from './context/PostsContext'
 import Footer from './components/Footer/Footer'
 import PostPage from './pages/PostPage'
+import Category from './pages/Category'
+import BannerHuman from './assets/banner-human.jpg'
+import BannerPet from './assets/banner-pet.jpg'
+import BannerMammal from './assets/banner-mammal.jpg'
+import BannerCanine from './assets/banner-canine.jpg'
 
 function App() {
 
@@ -16,6 +21,10 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/human' element={<Category title='With Humans' img={BannerHuman} tag='human'/>} />
+          <Route path='/pet' element={<Category title='Pets' img={BannerPet} tag='pet'/>} />
+          <Route path='/mammal' element={<Category title='Mammal' img={BannerMammal} tag='mammal'/>} />
+          <Route path='/canine' element={<Category title='Canine' img={BannerCanine} tag='canine'/>} />
           <Route path='/post/:id' element={<PostPage />}/>
         </Routes>
         <Footer />
