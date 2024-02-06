b2acd584a4a6d10cce5cb40951b5721922caebaf
Luiz Serão <luiz.serao@hotmail.com>
2024-02-06 16:58:46 -0300

    redirecionamento para páginas nos botões da página home

--- src/components/BannerCommunity/BannerCommunity.tsx
+++ src/components/BannerCommunity/BannerCommunity.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './BannerCommunity.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const BannerCommunity = () => {
+  const navigate = useNavigate()
+
   return (
     <section className={styles.community}>
       <div className={styles.bgEffect}></div>
@@ -9,7 +12,7 @@ const BannerCommunity = () => {
         <span className={styles.tag}>Dogs</span>
         <h1 className={styles.title}>Where Love and Barking Meet!</h1>
         <p className={styles.text}>Welcome to our Dog Community, where every bark is an expression of joy and every wagging tail is a dance of love!</p>
-        <button className={styles.start}>Send a message</button>
+        <button onClick={() => navigate('/pet')} className={styles.start}>Send a message</button>
       </div>
     </section>
   )
--- src/components/WelcomeHome/WelcomeHome.tsx
+++ src/components/WelcomeHome/WelcomeHome.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './WelcomeHome.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const WelcomeHome = () => {
+  const navigate = useNavigate()
+
   return (
     <div className={styles.welcomeHome}>
       <h1>Exploring the Dog World with Love and Joy!</h1>
       <p> The virtual corner dedicated to our four-legged friends and the incredible canine universe!</p>
-      <button>Explore more</button>
+      <button onClick={() => navigate('/mammal')}>Explore more</button>
     </div>
   )
 }
